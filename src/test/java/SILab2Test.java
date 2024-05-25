5dd5e96261539155f25445cdd8fe9da3d893cf7a
Blagoja Karakashev <154978538+BlagojaK03@users.noreply.github.com>
2024-05-25 03:42:58 +0200

    Add project files

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class SILab2Test {
+
+    @Test
+    public void EveryBranchTest() {
+
+    }
+
+    @Test
+    public void MultipleConditionTest() {
+        
+    }
+}
